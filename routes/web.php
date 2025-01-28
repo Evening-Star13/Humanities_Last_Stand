e051e41a8414ec4c95705f10d4860c7d4cb423d8
Chris Barranger <chrisbarranger@techie.com>
2025-01-28 15:08:27 -0400

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LivestockController;
 
 Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/ninjas', function () {
-  $ninjas = [
-    ['first-name' => 'Chris', 'last-name' => 'Barranger', 'gender' => 'Male', 'id' => '1'],
-    ['first-name' => 'Rebecca', 'last-name' => 'Barranger', 'gender' => 'Female', 'id' => '2'],
-    ['first-name' => 'Devin', 'last-name' => 'Patton', 'gender' => 'Male', 'id' => '3'],
-    ['first-name' => 'Jason', 'last-name' => 'Borden', 'gender' => 'Male', 'id' => '4'],
-  ];
-
-  return view('ninjas.index', ["greeting" => "Hello", "ninjas" => $ninjas]);
-});
+Route::get('/ninjas', [LivestockController::class, 'index']);
 
 Route::get('/ninjas/create', function () {
   return view('ninjas.create');
