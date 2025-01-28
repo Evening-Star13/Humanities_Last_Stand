98ffca1f7eae20d76a6922c29771259948447f34
Chris Barranger <chrisbarranger@techie.com>
2025-01-27 20:09:52 -0400

    added css styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"><html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Citizen Registry</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Citizen Registry</title>
+
+  @vite('resources/css/app.css')
 </head>
-<body>
+<body class='px-8 py-12 text-center'>
   <h1>Welcome to the Human Registry Correspondence System</h1>
-	<p>Click the Button below to self Register</p>
-	<a href="/ninjas" class="btn">
-		Register
-	</a>
+  <p>Click the Button below to self Register</p>
+  <a href="/ninjas" class="inline-block mt-4 btn">
+    Register
+  </a>
 </body>
 </html>
